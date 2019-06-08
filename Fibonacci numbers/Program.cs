17f9c8afe38b9b9633f07566eea3b1d5a5fb045e
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-06-08 15:59:58 +0400

    Fibonacci

---
+++ Fibonacci numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long fib;
+            long a = 1;
+            long b = 1;
+            Console.WriteLine("Press space for next number!");
+            for (; ; )
+            {
+                fib = a + b;
+                Console.Write(fib + "   ");
+                b = a;
+                a = fib;
+                Console.ReadKey();
+            }
+        }
+    }
+}
